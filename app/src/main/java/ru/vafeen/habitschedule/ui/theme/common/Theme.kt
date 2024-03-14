2318c465bd3dec44cf02719b2e1deed751629435
Vafeen <666av6@gmail.com>
2024-03-14 22:57:03 +0300

    Add common theme

---
+++ app/src/main/java/ru/vafeen/habitschedule/ui/theme/common/Theme.kt
@@ -0,0 +1,62 @@
+package ru.vafeen.habitschedule.ui.theme.common
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+data class HabitScheduleColors(
+    val background: Color,
+
+)
+
+val baseLightPalette = HabitScheduleColors(
+    background = Color.White,
+
+)
+
+val baseDarkPalette = baseLightPalette.copy(
+    background = Color.Black,
+)
+
+@Composable
+fun HabitScheduleTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    val colors = if (darkTheme) {
+        baseDarkPalette
+    } else {
+        baseLightPalette
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
+    CompositionLocalProvider(
+        LocalColors provides colors,
+        content = content
+    )
+}
+
+object HabitScheduleTheme {
+    val colors: HabitScheduleColors
+        @Composable
+        get() = LocalColors.current
+}
+
+val LocalColors = staticCompositionLocalOf<HabitScheduleColors> {
+    error("Composition error")
+}
