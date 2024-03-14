5576040d02024e6091c2e8f223e147f9dd268c3b
Vafeen <666av6@gmail.com>
2024-03-14 22:38:02 +0300

    Add fun getListFrequency whose purpose is obvious

--- app/src/main/java/ru/vafeen/habitschedule/noui/Frequency.kt
+++ app/src/main/java/ru/vafeen/habitschedule/noui/Frequency.kt
@@ -2,8 +2,15 @@ package ru.vafeen.habitschedule.noui
 
 enum class Frequency(val index: Int) {
     Once(0),
-    EveryDay(1),
+    EveryDay(1), // here will be time in millis
     EveryWeek(2),
     EveryMonth(3),
-    EveryYear(4)
+    EveryYear(4),
+    Modified(5);
+
+    companion object {
+        fun getListFrequency(): List<Frequency> {
+            return listOf(Once, EveryDay, EveryWeek, EveryMonth, EveryYear, Modified)
+        }
+    }
 }
