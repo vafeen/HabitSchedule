718c803faf05916ae935c5777bc90fb6272f7413
Vafeen <666av6@gmail.com>
2024-03-14 11:11:28 +0300

    Divide showNotification into showShortNotification and showLongNotification for short and long tasks

--- app/src/main/java/ru/vafeen/habitschedule/noui/notifications/NotificationService.kt
+++ app/src/main/java/ru/vafeen/habitschedule/noui/notifications/NotificationService.kt
@@ -1,9 +1,6 @@
 package ru.vafeen.habitschedule.noui.notifications
 
-import android.annotation.SuppressLint
-import android.app.Application
 import android.app.Notification
-import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.app.PendingIntent
@@ -13,10 +10,10 @@ import androidx.core.app.NotificationCompat
 import ru.vafeen.habitschedule.R
 import ru.vafeen.habitschedule.main.MainActivity
 
-val NOTIFICATION_CHANNEL_ID = "HabitScheduleID"
-val NOTIFICATION_CHANNEL_NAME = "HabitScheduleChannel"
+const val NOTIFICATION_CHANNEL_ID = "HabitScheduleID"
+const val NOTIFICATION_CHANNEL_NAME = "Напоминания"
 var NOTIFICATION_ID = 0
-val REQUEST_CODE = 200
+const val REQUEST_CODE = 200
 
 
 class NotificationService(
@@ -25,25 +22,39 @@ class NotificationService(
     private val notificationManager =
         context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-    private val myIntent = Intent(context, MainActivity::class.java)
-
     private val pendingIntent = PendingIntent.getActivity(
-        context, REQUEST_CODE,
-        myIntent,
+        context,
+        REQUEST_CODE,
+        Intent(context, MainActivity::class.java),
         PendingIntent.FLAG_IMMUTABLE
     )
 
-    fun showNotification(title: String, text: String) {
-        val notification: Notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle(title)
-//                .setContentText(text)
-            .setStyle(NotificationCompat.BigTextStyle().bigText(text))
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
-            .build()
+    fun showShortNotification(title: String, shortText: String) {
+        val notification: Notification =
+            NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setContentText(shortText)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .build()
 
         notificationManager.notify(NOTIFICATION_ID, notification)
         NOTIFICATION_ID += 1
     }
+
+    fun showLongNotification(title: String, bigText: String) {
+        val notification: Notification =
+            NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(title)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(bigText))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .build()
+
+        notificationManager.notify(NOTIFICATION_ID, notification)
+        NOTIFICATION_ID += 1
+    }
+
 }
