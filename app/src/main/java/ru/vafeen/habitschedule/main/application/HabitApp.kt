ca78b68be14fe1521abdacd2322a57a1bc7ec524
Vafeen <666av6@gmail.com>
2024-03-15 23:17:32 +0300

    fix database

--- app/src/main/java/ru/vafeen/habitschedule/main/application/HabitApp.kt
+++ app/src/main/java/ru/vafeen/habitschedule/main/application/HabitApp.kt
@@ -4,13 +4,23 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import androidx.room.Room
+import ru.vafeen.habitschedule.noui.db.HabitScheduleDatabase
 import ru.vafeen.habitschedule.noui.notifications.NOTIFICATION_CHANNEL_ID
 import ru.vafeen.habitschedule.noui.notifications.NOTIFICATION_CHANNEL_NAME
 
 class HabitApp : Application() {
+    companion object {
+        var hSDB: HabitScheduleDatabase? = null
+            private set
+    }
 
     override fun onCreate() {
         super.onCreate()
+        hSDB = Room
+            .databaseBuilder(this, HabitScheduleDatabase::class.java, "HabitSchedule.db")
+            .build()
+
         val notificationManager =
             applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
@@ -21,4 +31,5 @@ class HabitApp : Application() {
         )
         notificationManager.createNotificationChannel(notificationChannel)
     }
+
 }
