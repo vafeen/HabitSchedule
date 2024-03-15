2536d1709ddc994319cf48363d12104f560811b3
Vafeen <666av6@gmail.com>
2024-03-15 23:20:24 +0300

    Add Data screen to bottom bar

--- app/src/main/java/ru/vafeen/habitschedule/ui/common/components/bottom_bar/BottomBar.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/common/components/bottom_bar/BottomBar.kt
@@ -3,6 +3,7 @@ package ru.vafeen.habitschedule.ui.common.components.bottom_bar
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
@@ -14,12 +15,19 @@ import ru.vafeen.habitschedule.ui.screens.Screens
 @Composable
 fun BottomBar(
     navHostController: NavHostController,
-    selected1: Boolean,
+    selected1: Boolean = false,
+    selected2: Boolean = false,
 ) {
     BottomAppBar(modifier = Modifier.fillMaxSize()) {
+
         NavigationBarItem(
             selected = selected1,
             onClick = { navHostController.navigate(Screens.Main.route) },
             icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "home screen") })
+
+        NavigationBarItem(
+            selected = selected2,
+            onClick = { navHostController.navigate(Screens.Data.route) },
+            icon = { Icon(imageVector = Icons.Default.List, contentDescription = "data screen") })
     }
 }
