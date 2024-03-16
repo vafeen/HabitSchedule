cd2c89f54239d7a1ab21275f47e4d0345be97fed
Vafeen <666av6@gmail.com>
2024-03-16 19:44:47 +0300

    Add size of icons in BottomBar

--- app/src/main/java/ru/vafeen/habitschedule/ui/common/components/bottom_bar/BottomBar.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/common/components/bottom_bar/BottomBar.kt
@@ -1,6 +1,9 @@
 package ru.vafeen.habitschedule.ui.common.components.bottom_bar
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
@@ -9,6 +12,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import ru.vafeen.habitschedule.ui.screens.Screens
 
@@ -19,16 +23,30 @@ fun BottomBar(
     onClickToScreen1: () -> Unit = {},
     onClickToScreen2: () -> Unit = {},
 ) {
-    BottomAppBar(modifier = Modifier.fillMaxSize()) {
+    val sizeOfIcons = 30.dp
+    BottomAppBar(modifier = Modifier
+        .fillMaxWidth()
+        .height(50.dp)) {
+
 
         NavigationBarItem(
             selected = selected1,
-            onClick =  onClickToScreen1,
-            icon = { Icon(imageVector = Icons.Default.Home, contentDescription = "home screen") })
+            onClick = onClickToScreen1,
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home, contentDescription = "home screen",
+                    modifier = Modifier.size(sizeOfIcons)
+                )
+            })
 
         NavigationBarItem(
             selected = selected2,
             onClick = onClickToScreen2,
-            icon = { Icon(imageVector = Icons.Default.List, contentDescription = "data screen") })
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.List, contentDescription = "data screen",
+                    modifier = Modifier.size(sizeOfIcons)
+                )
+            })
     }
 }
