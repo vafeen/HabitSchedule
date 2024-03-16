b0488e59f9bb0893a5c977f523526a89f5b9531e
Vafeen <666av6@gmail.com>
2024-03-16 19:43:52 +0300

    Add ruName for frequency

--- app/src/main/java/ru/vafeen/habitschedule/noui/Frequency.kt
+++ app/src/main/java/ru/vafeen/habitschedule/noui/Frequency.kt
@@ -1,12 +1,12 @@
 package ru.vafeen.habitschedule.noui
 
-enum class Frequency(val index: Int) {
-    Once(0),
-    EveryDay(1), // here will be time in millis
-    EveryWeek(2),
-    EveryMonth(3),
-    EveryYear(4),
-    Modified(5);
+enum class Frequency(val index: Int, val ruName: String) {
+    Once(index = 0, ruName = "Разово"),
+    EveryDay(index = 1, ruName = "Каждый день"), // here will be time in millis
+    EveryWeek(index = 2, ruName = "Каждую неделю"),
+    EveryMonth(index = 3, ruName = "Каждый месяц"),
+    EveryYear(index = 4, ruName = "Каждый год"),
+    Modified(index = 5, ruName = "Другое");
 
     companion object {
         fun getListFrequency(): List<Frequency> {
