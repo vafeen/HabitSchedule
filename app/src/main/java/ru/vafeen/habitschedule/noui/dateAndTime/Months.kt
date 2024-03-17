b9a9e2c868af65036968e9ea01f2c9f850a85a7b
Vafeen <666av6@gmail.com>
2024-03-17 14:33:08 +0300

    Add ru names of moths

---
+++ app/src/main/java/ru/vafeen/habitschedule/noui/dateAndTime/Months.kt
@@ -0,0 +1,18 @@
+package ru.vafeen.habitschedule.noui.dateAndTime
+
+fun getRuMonths(): List<String> {
+    return listOf(
+        "янв.",
+        "фев.",
+        "мар.",
+        "апр.",
+        "мая",
+        "июн.",
+        "июл.",
+        "авг.",
+        "сен.",
+        "окт.",
+        "ноя.",
+        "дек.",
+    )
+}
