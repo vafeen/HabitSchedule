b322a531d0df076805a1c8c3311f921ba7e30653
Vafeen <666av6@gmail.com>
2024-03-17 21:41:12 +0300

    Modify Main Screen

--- app/src/main/java/ru/vafeen/habitschedule/ui/screens/main/Main.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/screens/main/Main.kt
@@ -6,16 +6,45 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import androidx.room.Room
+import ru.vafeen.habitschedule.main.application.HabitApp
+import ru.vafeen.habitschedule.noui.db.HabitScheduleDatabase
+import ru.vafeen.habitschedule.noui.db.HabitScheduleRepository
+import ru.vafeen.habitschedule.ui.common.components.bottom_bar.BottomBar
+import ru.vafeen.habitschedule.ui.screens.Screens
 
 @Composable
-fun Main() {
+fun Main(
+    navHostController: NavHostController,
+) {
+    var db: HabitScheduleRepository? = null
+    val itemDao = HabitApp.hSDB?.habitItemDao()
+    val dtDao = HabitApp.hSDB?.habitDateTimeDao()
+    if (itemDao != null && dtDao != null) {
+        db = HabitScheduleRepository(
+            itemDao = itemDao,
+            dtDao = dtDao
+        )
+    }
+    if (db != null) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            bottomBar = {
+                BottomBar(selected1 = true,
+                    onClickToScreen2 = {
+                        navHostController.navigate(Screens.Data.route)
+                    })
+            },
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize()
-    ) { innerPadding ->
-        Column(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)) {
+            }
 
         }
 
