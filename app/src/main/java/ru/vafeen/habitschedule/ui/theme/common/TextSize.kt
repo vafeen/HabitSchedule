414256f106b001c9c91bb17474c84722dd3f9926
Vafeen <666av6@gmail.com>
2024-03-17 23:19:42 +0300

    Add TextSize

---
+++ app/src/main/java/ru/vafeen/habitschedule/ui/theme/common/TextSize.kt
@@ -0,0 +1,11 @@
+package ru.vafeen.habitschedule.ui.theme.common
+
+import androidx.compose.ui.unit.sp
+
+object TextSize {
+    val extraSmall = 10.sp
+    val small = 15.sp
+    val medium = 17.sp
+    val big = 19.sp
+    val huge = 22.sp
+}
