042c95b5a6f1f8ae1ae2866bcc5d19b2f553a1bd
Vafeen <666av6@gmail.com>
2024-03-20 20:19:08 +0300

    Modify adding habit dialog

--- app/src/main/java/ru/vafeen/habitschedule/ui/common/components/dialogs/AddingHabitDialog.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/common/components/dialogs/AddingHabitDialog.kt
@@ -4,16 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,13 +27,10 @@ import androidx.compose.ui.window.DialogProperties
 import ru.vafeen.habitschedule.noui.HabitItem
 import ru.vafeen.habitschedule.ui.common.components.date_picker.HSDatePicker
 import ru.vafeen.habitschedule.ui.common.components.date_picker.isTrueDateAndTime
-import ru.vafeen.habitschedule.ui.common.components.time_input.HSTimeInput
+import ru.vafeen.habitschedule.ui.common.components.time_input.HSTimePicker
 import ru.vafeen.habitschedule.ui.theme.common.HabitScheduleTheme
-import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddingHabitDialog(
     onDismissRequest: () -> Unit, onAddNewItem: (item: HabitItem) -> Unit,
@@ -47,19 +44,16 @@ fun AddingHabitDialog(
         unfocusedContainerColor = HabitScheduleTheme.colors.background,
         focusedContainerColor = HabitScheduleTheme.colors.background,
     )
-    val timePicketState = rememberTimePickerState(
-        is24Hour = true
-    )
-    val datePickerState = remember {
-        mutableStateOf(LocalDate.now())
+    val dateTimePickerState = remember {
+        mutableStateOf(LocalDateTime.now())
     }
+
     Dialog(
         onDismissRequest = onDismissRequest,
         properties = DialogProperties(usePlatformDefaultWidth = false)
     ) {
         Column(
             modifier = Modifier
-//            .padding(paddingValues = paddingValues)
                 .size(500.dp)
                 .background(HabitScheduleTheme.colors.background)
                 .verticalScroll(rememberScrollState()),
@@ -73,28 +67,34 @@ fun AddingHabitDialog(
                 label = { Text(text = "Название") },
                 singleLine = true
             )
+
+            Spacer(modifier = Modifier.height(5.dp))
+
             OutlinedTextField(
                 value = newItem.text,
                 onValueChange = { newItem = newItem.copy(text = it) },
                 colors = tfColors,
                 label = { Text(text = "Описание") }
             )
-            HSTimeInput(state = timePicketState)
-            HSDatePicker(state = datePickerState)
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            HSTimePicker(state = dateTimePickerState)
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            HSDatePicker(state = dateTimePickerState)
+
+            Spacer(modifier = Modifier.height(5.dp))
+
             Row(modifier = Modifier.fillMaxWidth()) {
                 Button(
                     onClick = {
                         newItem = newItem.copy(
-                            dateTime = LocalDateTime.of(
-                                datePickerState.value,
-                                LocalTime.of(timePicketState.hour, timePicketState.minute)
-                            )
+                            dateTime = dateTimePickerState.value
                         )
                     },
-                    enabled = isTrueDateAndTime(
-                        datePickerState = datePickerState,
-                        timePicketState = timePicketState,
-                    )
+                    enabled = isTrueDateAndTime(dateTimePickerState = dateTimePickerState)
                 ) {
                     Text(text = "add")
                 }
