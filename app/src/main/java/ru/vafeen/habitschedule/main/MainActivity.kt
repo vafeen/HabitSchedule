f6a1350f05b7b3e357bea17b69ad9694bf968d30
Vafeen <666av6@gmail.com>
2024-03-20 20:19:35 +0300

    Fix bug with MainActivity

--- app/src/main/java/ru/vafeen/habitschedule/main/MainActivity.kt
+++ app/src/main/java/ru/vafeen/habitschedule/main/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import ru.vafeen.habitschedule.noui.permissions.RequestNotificationPermission
-import ru.vafeen.habitschedule.ui.navigation.HabitScheduleNavHost
+import ru.vafeen.habitschedule.ui.common.navigation.HabitScheduleNavHost
 import ru.vafeen.habitschedule.ui.theme.common.HabitScheduleTheme
 
 class MainActivity : ComponentActivity() {
