b3d1ec5bd57ebf7172a17f74be8463d24acf1c40
Vafeen <666av6@gmail.com>
2024-03-20 21:34:12 +0300

    Modify date picker

--- app/src/main/java/ru/vafeen/habitschedule/ui/common/components/date_picker/HSDatePicker.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/common/components/date_picker/HSDatePicker.kt
@@ -1,6 +1,5 @@
 package ru.vafeen.habitschedule.ui.common.components.date_picker
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,43 +10,35 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
-import androidx.compose.material3.DatePicker
-import androidx.compose.material3.DatePickerFormatter
-import androidx.compose.material3.DatePickerState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import ru.vafeen.habitschedule.noui.dateAndTime.getRuDaysOfWeek
 import ru.vafeen.habitschedule.noui.dateAndTime.getRuMonths
 import ru.vafeen.habitschedule.ui.theme.common.HabitScheduleTheme
 import ru.vafeen.habitschedule.ui.theme.common.TextSize
-import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.ZoneId
 
-@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun HSDatePicker(
     state: MutableState<LocalDateTime>,
 ) {
-    val cor = rememberCoroutineScope()
     val lazyListState = rememberLazyListState()
     val today = LocalDateTime.now()
-    cor.launch {
+
+    LaunchedEffect(key1 = null) {
         lazyListState.scrollToItem(index = today.dayOfYear - state.value.dayOfYear)
     }
+
     val ruDaysOfWeek = getRuDaysOfWeek()
+
     val ruMonths = getRuMonths()
+
     LazyColumn(
         state = lazyListState,
         modifier = Modifier.height(100.dp),
