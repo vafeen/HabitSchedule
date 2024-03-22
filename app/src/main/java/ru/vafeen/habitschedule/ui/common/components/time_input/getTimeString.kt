67d40b16771d2fc8064c352ada1b5e577a8fc920
Vafeen <666av6@gmail.com>
2024-03-22 22:07:11 +0300

    Modify getTimeString

--- app/src/main/java/ru/vafeen/habitschedule/ui/common/components/time_input/getTimeString.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/common/components/time_input/getTimeString.kt
@@ -1,8 +1,16 @@
 package ru.vafeen.habitschedule.ui.common.components.time_input
 
+import java.time.LocalDateTime
 import java.time.LocalTime
 
 fun LocalTime.getTimeString(): String = "$hour:${
+    if (minute > 9) {
+        minute
+    } else {
+        "0$minute"
+    }
+}"
+fun LocalDateTime.getTimeString(): String = "$hour:${
     if (minute > 9) {
         minute
     } else {
