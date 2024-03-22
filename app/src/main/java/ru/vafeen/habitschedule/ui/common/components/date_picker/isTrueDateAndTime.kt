83a4bec80ac2e415698edf5f05f1242001213535
Vafeen <666av6@gmail.com>
2024-03-22 21:31:27 +0300

    Fix name of package

--- app/src/main/java/ru/vafeen/habitschedule/ui/common/components/date_picker/isTrueDateAndTime.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/common/components/date_picker/isTrueDateAndTime.kt
@@ -1,10 +1,7 @@
 package ru.vafeen.habitschedule.ui.common.components.date_picker
 
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.MutableState
-import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.LocalTime
 
 fun isTrueDateAndTime(
     dateTimePickerState: MutableState<LocalDateTime>,
