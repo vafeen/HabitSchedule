ebf5968b025f38933d5e46e3627f444b5534c4af
Vafeen <666av6@gmail.com>
2024-03-22 20:43:38 +0300

    Modify TimePickerItemButton

--- app/src/main/java/ru/vafeen/habitschedule/ui/common/components/time_input/TimePickerItemButton.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/common/components/time_input/TimePickerItemButton.kt
@@ -2,10 +2,16 @@ package ru.vafeen.habitschedule.ui.common.components.time_input
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import ru.vafeen.habitschedule.ui.theme.common.HabitScheduleTheme
+import ru.vafeen.habitschedule.ui.theme.common.TextSize
 
 
 @Composable
@@ -15,11 +21,23 @@ fun TimePickerItemButton(
     modifier: Modifier = Modifier,
     text: String,
 ) {
-    Button(
+
+    TextButton(
         modifier = modifier.padding(horizontal = 2.dp),
         onClick = onClick,
-        enabled = enabled
+        enabled = enabled,
+        colors = ButtonDefaults.textButtonColors(
+            containerColor = HabitScheduleTheme.colors.background,
+            contentColor = HabitScheduleTheme.colors.blackInLightAndLightGrayInDark,
+            disabledContentColor = HabitScheduleTheme.colors.blackInLightAndLightGrayInDark,
+            disabledContainerColor = HabitScheduleTheme.colors.barsColor
+        )
     ) {
-        Text(text = text)
+        Text(
+            text = text,
+            fontSize = TextSize.medium
+//            color = Color.Red
+        )
     }
+
 }
