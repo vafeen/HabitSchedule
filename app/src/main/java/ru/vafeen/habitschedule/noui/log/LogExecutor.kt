99dacb527d83686654fdecc3dd6b73b2c9734358
Vafeen <666av6@gmail.com>
2024-03-24 20:13:24 +0300

    Modify logExecutor with adding LogType

--- app/src/main/java/ru/vafeen/habitschedule/noui/log/LogExecutor.kt
+++ app/src/main/java/ru/vafeen/habitschedule/noui/log/LogExecutor.kt
@@ -2,10 +2,20 @@ package ru.vafeen.habitschedule.noui.log
 
 import android.util.Log
 
-fun logExecutor(tag: String, message: String) {
-    val show = true
-    if (show) {
-        Log.e(tag, message)
+fun logExecutor(suffixTag: String, message: String) {
+
+    // "when" for switching on/off some types of logs :)
+    when (suffixTag) {
+
+        LogType.Database.value -> {
+            Log.e(LogType.defaultName + suffixTag, message)
+        }
+
+        else -> {
+            Log.e(suffixTag, message)
+        }
     }
+
+
 
 }
---
+++ app/src/main/java/ru/vafeen/habitschedule/noui/log/LogType.kt
@@ -0,0 +1,10 @@
+package ru.vafeen.habitschedule.noui.log
+
+enum class LogType(val value: String) {
+    Database("Database");
+
+    companion object {
+        const val defaultName = "hs"
+    }
+
+}
