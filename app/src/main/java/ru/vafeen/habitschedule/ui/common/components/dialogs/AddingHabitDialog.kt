40d105a74f4098fc3f5e5e4a4c02ec348b208dbc
Vafeen <666av6@gmail.com>
2024-04-16 02:48:55 +0300

    Modify AddingHabitDialog.kt. Fix updating

--- app/src/main/java/ru/vafeen/habitschedule/ui/common/components/dialogs/AddingHabitDialog.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/common/components/dialogs/AddingHabitDialog.kt
@@ -1,11 +1,13 @@
 package ru.vafeen.habitschedule.ui.common.components.dialogs
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import ru.vafeen.habitschedule.noui.HabitItem
 import ru.vafeen.habitschedule.noui.dateAndTime.getRuMonths
 import ru.vafeen.habitschedule.ui.common.components.time_input.getTimeString
+import ru.vafeen.habitschedule.ui.theme.common.HabitScheduleTheme
 
 @Composable
 fun AddingHabitDialog(
@@ -23,7 +25,11 @@ fun AddingHabitDialog(
         mainButtonAction = onAddNewItem,
         mainButtonText = { item ->
             val dt = item.dateTime
-            "Отправить ${dt.dayOfMonth} ${ruMonths[dt.monthValue]} ${dt.year} в ${dt.getTimeString()}"
+            Text(
+                text = "Отправить ${dt.dayOfMonth} " +
+                        "${ruMonths[dt.monthValue - 1]} ${dt.year} в ${dt.getTimeString()}",
+                color = HabitScheduleTheme.colors.blackInLightAndLightGrayInDark
+            )
         }
     )
 
