75eb5f7c8b574db67375ca1c68930d189a8b1cf3
Vafeen <666av6@gmail.com>
2024-04-25 02:00:23 +0300

    Add annotaion

--- app/src/main/java/ru/vafeen/habitschedule/noui/dateAndTime/getMonth.kt
+++ app/src/main/java/ru/vafeen/habitschedule/noui/dateAndTime/getMonth.kt
@@ -2,6 +2,7 @@ package ru.vafeen.habitschedule.noui.dateAndTime
 
 import java.time.Month
 
+@OptIn(ExperimentalStdlibApi::class)
 fun Int.getMonth(): Month {
     return try {
         Month.entries[this]
