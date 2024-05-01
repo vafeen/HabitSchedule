5d4df6bc2b1b0330c407b67d9a53408d1610b30c
Vafeen <666av6@gmail.com>
2024-05-01 05:23:12 +0300

    Modify FrequencyPicker

--- app/src/main/java/ru/vafeen/habitschedule/noui/Frequency.kt
+++ app/src/main/java/ru/vafeen/habitschedule/noui/Frequency.kt
@@ -2,15 +2,15 @@ package ru.vafeen.habitschedule.noui
 
 enum class Frequency(val index: Int, val ruName: String) {
     Once(index = 0, ruName = "Разово"),
-    EveryDay(index = 1, ruName = "Каждый день"), // here will be time in millis
-    EveryWeek(index = 2, ruName = "Каждую неделю"),
-    EveryMonth(index = 3, ruName = "Каждый месяц"),
-    EveryYear(index = 4, ruName = "Каждый год"),
+    Daily(index = 1, ruName = "Каждый день"), // here will be time in millis
+    Weekly(index = 2, ruName = "Каждую неделю"),
+    Monthly(index = 3, ruName = "Каждый месяц"),
+    Yearly(index = 4, ruName = "Каждый год"),
     Modified(index = 5, ruName = "Другое");
 
     companion object {
         fun getListFrequency(): List<Frequency> {
-            return listOf(Once, EveryDay, EveryWeek, EveryMonth, EveryYear, Modified)
+            return listOf(Once, Daily, Weekly, Monthly, Yearly, Modified)
         }
     }
 }
--- app/src/main/java/ru/vafeen/habitschedule/ui/common/components/frequency_picker/FrequencyPicker.kt
+++ app/src/main/java/ru/vafeen/habitschedule/ui/common/components/frequency_picker/FrequencyPicker.kt
@@ -1,25 +1,46 @@
 package ru.vafeen.habitschedule.ui.common.components.frequency_picker
 
-import android.service.autofill.OnClickAction
+import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import ru.vafeen.habitschedule.noui.Frequency
+import ru.vafeen.habitschedule.noui.FrequencyData
+import ru.vafeen.habitschedule.noui.HabitItem
+import ru.vafeen.habitschedule.noui.dateAndTime.getRuDaysOfWeek
+import ru.vafeen.habitschedule.ui.common.components.days_picker.HSDaysPickerButton
 
 @Composable
 fun FrequencyPicker(
     initialValue: Frequency,
-    onClickAction: (Frequency) -> Unit,
+    item: MutableState<HabitItem>
 ) {
     var frequency by remember {
         mutableStateOf(initialValue)
+    }
+
+    val daysOfWeek = getRuDaysOfWeek()
+
+    var numbers by remember {
+        mutableStateOf(listOf<Int>())
     }
 
     var isExpanded by remember {
@@ -29,18 +50,91 @@ fun FrequencyPicker(
     val onDismissRequest = { isExpanded = false }
 
     val freqList = Frequency.getListFrequency()
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Очередность события:")
 
-    Box() {
-        Text(text = frequency.ruName)
+        Spacer(modifier = Modifier.height(5.dp))
 
-        DropdownMenu(expanded = isExpanded, onDismissRequest = onDismissRequest) {
-            for (frequencyI in freqList) {
-                Text(text = frequencyI.ruName, modifier = Modifier.clickable {
-                    frequency = frequencyI
-                    onDismissRequest()
-                    onClickAction(frequencyI)
-                })
+        Box() {
+            Text(text = frequency.ruName, modifier = Modifier.clickable {
+                isExpanded = !isExpanded
+            })
+
+            DropdownMenu(expanded = isExpanded, onDismissRequest = onDismissRequest) {
+                for (frequencyI in freqList) {
+                    Text(text = frequencyI.ruName, modifier = Modifier.clickable {
+                        frequency = frequencyI
+
+                        onDismissRequest()
+
+                        item.value = item.value.copy(
+                            frequencyData = FrequencyData(
+                                frequency = frequencyI,
+                                listOfNumbers = null
+                            )
+                        )
+                    })
+                }
+            }
+        }
+
+        when (frequency) {
+            Frequency.Weekly -> {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    for (indexOfDay in 0..daysOfWeek.lastIndex) {
+                        HSDaysPickerButton(
+                            day = "$indexOfDay",
+                            checked = indexOfDay in numbers
+                        ) {
+                            numbers = when (it) {
+                                true -> {
+                                    numbers.plus(indexOfDay)
+                                }
+
+                                false -> {
+                                    numbers.minus(indexOfDay)
+                                }
+                            }
+                            Log.d("APPP", "$numbers")
+
+                        }
+                    }
+                }
+            }
+
+            Frequency.Monthly -> {
+                LazyVerticalGrid(columns = GridCells.Fixed(7)) {
+                    items(count = 31) { index ->
+                        HSDaysPickerButton(
+                            day = "$index",
+                            checked = index in numbers
+                        ) {
+                            numbers = when (it) {
+                                true -> {
+                                    numbers.minus(index)
+                                }
+
+                                false -> {
+                                    numbers.plus(index)
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            else -> {
+
             }
         }
     }
+
+
 }
